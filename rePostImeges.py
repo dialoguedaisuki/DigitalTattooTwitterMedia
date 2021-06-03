d5ff78c471f223949b04dde37280cbbff6bdff9f
natsukawadaisuki <60532865+natsukawadaisuki@users.noreply.github.com>
2021-06-03 22:01:33 +0900

    #1 modify,refactoring

--- rePostImeges.py
+++ rePostImeges.py
@@ -4,6 +4,7 @@ from pprint import pprint
 import re
 from io import BytesIO
 import requests
+import csv
 
 # get args
 search_words, envName = args()
@@ -16,27 +17,35 @@ def main():
     # Extracting text and original URL
     copyIdAndImege = []
     idList = simple_tweet_search(search_words, envName)
-    print("---------------------target")
+    print("---------------------search target")
     print(idList)
-    print("---------------------excluded")
+    # Exclude already been posted and
+    print("---------------------noTweetIds")
     with open(csvname) as f:
         noTweetIds = [int(s.strip()) for s in f.readlines()]
     print(noTweetIds)
-    for id in idList:
-        if id not in noTweetIds:
-            try:
-                tweet = api.get_status(id)
-            except Exception as e:
-                print(e)
-            ret = re.sub(
-                r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+)", "", tweet.text).replace('@', '')[0:100]
-            # ret += f'\by {tweet.user.screen_name}'
-            ret += f'\n by https://twitter.com/{tweet.user.screen_name}'
-            try:
-                copyIdAndImege.append([ret, [i['media_url']
-                                             for i in tweet.extended_entities['media'] if i['type'] != 'video']])
-            except Exception as e:
-                print(e)
+    # Exclude images of followers
+    meId = api.me().screen_name
+    followerIds = api.followers_ids(meId)
+    print("---------------------followerIds")
+    print(followerIds)
+    twIds = [i for i in idList if i not in noTweetIds and i not in followerIds]
+    print("---------------------execution targets")
+    print(twIds)
+    # create post list
+    for id in twIds:
+        try:
+            tweet = api.get_status(id)
+        except Exception as e:
+            print(e)
+        ret = re.sub(
+            r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+)", "", tweet.text).replace('@', '')[0:100]
+        ret += f'\n by https://twitter.com/{tweet.user.screen_name}'
+        try:
+            copyIdAndImege.append([ret, [i['media_url']
+                                         for i in tweet.extended_entities['media'] if i['type'] != 'video']])
+        except Exception as e:
+            print(e)
     pprint(copyIdAndImege)
     # Upload and make post list
     uploadList = []
@@ -53,15 +62,17 @@ def main():
         if i[1] != []:
             try:
                 post = api.update_status(status=i[0], media_ids=i[1])
-                print("---------------------------------------------------------------post")
+                print(
+                    "---------------------------------------------------------------post")
                 pprint([post._json['created_at'], post._json['text']])
             except Exception as e:
                 print(e)
     # Record what you have already posted
-    strList = [str(i) for i in idList]
-    strId = '\n'.join(strList)
-    with open(csvname, 'wt') as f:
-        f.write(strId)
+    writeIds = [[i] for i in twIds]
+    with open(csvname, 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(writeIds)
+    pass
 
 
 if __name__ == "__main__":
--- rePostVideos.py
+++ rePostVideos.py
@@ -2,6 +2,7 @@ from tweetUtil import simple_tweet_search, auth_api, auth_api2, uploadVideo
 from args import args
 from pprint import pprint
 import re
+import csv
 
 # get args
 search_words, envName = args()
@@ -15,29 +16,37 @@ def main():
     # Extracting text and original URL
     copyIdAndImege = []
     idList = simple_tweet_search(search_words, envName)
-    print("---------------------target")
+    print("---------------------search target")
     print(idList)
-    print("---------------------excluded")
+    # Exclude already been posted and
+    print("---------------------noTweetIds")
     with open(csvname) as f:
         noTweetIds = [int(s.strip()) for s in f.readlines()]
     print(noTweetIds)
-    for id in idList:
-        if id not in noTweetIds:
-            try:
-                tweet = api.get_status(id)
-            except Exception as e:
-                print(e)
-            ret = re.sub(
-                r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+)", "", tweet.text).replace('@', '')[0:100]
-            # ret += f'\n by @{tweet.user.screen_name}'
-            ret += f'\n by https://twitter.com/{tweet.user.screen_name}'
-            try:
-                url = max([i['url'] for i in tweet.extended_entities['media'][0]['video_info']
-                           ['variants'] if i['content_type'] != 'application/x-mpegURL'])
-                copyIdAndImege.append(
-                    [ret, url])
-            except Exception as e:
-                print(e)
+    # Exclude images of followers
+    meId = api.me().screen_name
+    followerIds = api.followers_ids(meId)
+    print("---------------------followerIds")
+    print(followerIds)
+    twIds = [i for i in idList if i not in noTweetIds and i not in followerIds]
+    print("---------------------execution targets")
+    print(twIds)
+    # create post list
+    for id in twIds:
+        try:
+            tweet = api.get_status(id)
+        except Exception as e:
+            print(e)
+        ret = re.sub(
+            r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+$,%#]+)", "", tweet.text).replace('@', '')[0:100]
+        ret += f'\n by https://twitter.com/{tweet.user.screen_name}'
+        try:
+            url = max([i['url'] for i in tweet.extended_entities['media'][0]['video_info']
+                        ['variants'] if i['content_type'] != 'application/x-mpegURL'])
+            copyIdAndImege.append(
+                [ret, url])
+        except Exception as e:
+            print(e)
     pprint(copyIdAndImege)
     # Upload and make post list
     for i in copyIdAndImege:
@@ -46,10 +55,11 @@ def main():
         except Exception as e:
             print(e)
     # Record what you have already posted
-    strList = [str(i) for i in idList]
-    strId = '\n'.join(strList)
-    with open(csvname, 'wt') as f:
-        f.write(strId)
+    writeIds = [[i] for i in twIds]
+    with open(csvname, 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(writeIds)
+    pass
 
 
 if __name__ == "__main__":
