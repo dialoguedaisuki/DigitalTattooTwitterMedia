838d5bfd8c0bdec529e19e697c3037a29b3930da
user_name <mailhasimasen@nomail.com>
2022-02-23 19:45:36 +0900

    transfer cron job img -> django/static

--- getImage.sh
+++ getImage.sh
@@ -6,3 +6,4 @@ echo "db_envName=" $3
 mkdir -p ~/log/$1
 python3 ~/DigitalTattooTwitterMedia/tweetSearchDbInsert.py $1 $2 $3 2>&1 | tee ~/log/$1/`date "+%Y%m%d_%H%M%S"`.image.log
 python3 ~/DigitalTattooTwitterMedia/delFlagInsert $1 $3 2>&1 | tee ~/log/$1/`date "+%Y%m%d_%H%M%S"`.insDelFlag.log
+mv ~/DigitalTattooTwitterMedia/image/* ~/sugukesu_img/sugukesu_img/static/sugukesu_img/img
