23ce297496b1bf5c4019671f95ae9ae6c141a4f2
natsukawadaisuki <60532865+natsukawadaisuki@users.noreply.github.com>
2022-02-28 19:52:39 +0900

    view add

--- createTable.sql
+++ createTable.sql
@@ -17,3 +17,15 @@ create table image(
   image text, 
   foreign key (id) references info(id)
 );
+
+-- VIEW
+CREATE VIEW infoimage as 
+SELECT info.id,
+   info.create_at,
+   info.screen_name,
+   info.tweet_text,
+   info.bio,
+   image.image,
+   image.num,
+   info.delflag
+  FROM (info JOIN image ON ((info.id = image.id)));
