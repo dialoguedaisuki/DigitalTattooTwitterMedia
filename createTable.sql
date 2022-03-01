075982ade1d80a36890f4d179c1cfebfc57d99ce
natsukawadaisuki <60532865+natsukawadaisuki@users.noreply.github.com>
2022-03-01 19:55:21 +0900

    view filter

--- createTable.sql
+++ createTable.sql
@@ -19,8 +19,8 @@ create table image(
   foreign key (id) references info(id)
 );
 
--- VIEW
-CREATE VIEW infoimage as 
+-- RAW VIEW
+CREATE VIEW rawinfoimage as 
 SELECT info.id,
    info.create_at,
    info.screen_name,
@@ -29,4 +29,27 @@ SELECT info.id,
    image.image,
    image.num,
    info.delflag
-  FROM (info JOIN image ON ((info.id = image.id)));
+  FROM (info JOIN image ON ((info.id = image.id))) 
+
+-- FILTER VIEW
+CREATE VIEW infoimage as 
+SELECT info.id,
+   info.create_at,
+   info.screen_name,
+   info.tweet_text,
+   info.bio,
+   image.image,
+   image.num,
+   info.delflag,
+   image.score
+  FROM (info JOIN image ON ((info.id = image.id)))
+  WHERE bio NOT LIKE '%成人%' 
+  AND bio NOT LIKE '%絵%'
+  AND bio NOT LIKE '%創作%'
+  AND bio NOT LIKE '%雑多%'
+  AND bio NOT LIKE '%TRPG%'
+  AND bio NOT LIKE '%転載%'
+  AND bio NOT LIKE '%推%'
+  AND tweet_text NOT LIKE '%ラフ%'
+  AND tweet_text NOT LIKE '%描%'
+  AND tweet_text NOT LIKE '%下書%';
